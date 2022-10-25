87726a5485e19e8322fb0c579e9401e51984f5f1
NickJordan289 <nickjordan289@gmail.com>
2022-10-25 22:19:18 +0800

    CosmosDB to MongoDB, removed exposed ports

--- server/index.js
+++ server/index.js
@@ -1,80 +1,80 @@
 import express from "express";
 import { Network, Alchemy } from "alchemy-sdk";
-import { CosmosClient } from "@azure/cosmos";
 import * as util from "ethereumjs-util";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 const PORT = process.env.APP_SERVER_PORT || 4000;
-// const key = process.env.COSMOS_KEY;
-// const endpoint = process.env.COSMOS_ENDPOINT;
-const key =
-  "wJSlFra1ZyUXQDzM1AjnKziAcdGeTdX5wrsZ0oS6ZCNzVTRyYrZ4dVQNJDzo22FSm3HYANWFQYbj3Kc1FKANOg==";
-const endpoint = "https://unitynftservicedb.documents.azure.com:443/";
 
-// Set Database name and container name with unique timestamp
+// Initialize MongoDB and ensure the database and container exist
 const databaseName = `unitynftservice`;
 const containerName = `accounts`;
-const partitionKeyPath = ["/id"];
 
-const cosmosClient = new CosmosClient({ endpoint, key });
-
-// Create database if it doesn't exist
-const { database } = await cosmosClient.databases.createIfNotExists({
-  id: databaseName,
+const mongoClient = new MongoClient(process.env.MONGODB_CONNSTRING);
+const db = await mongoClient.connect().then((client) => {
+  return client.db(databaseName);
 });
-console.log(`${database.id} database ready`);
+console.log(`${databaseName} database ready`);
+const accounts = db.collection(containerName);
+console.log(`${containerName} container ready`);
 
-// Create container if it doesn't exist
-const { container } = await database.containers.createIfNotExists({
-  id: containerName,
-  partitionKey: {
-    paths: partitionKeyPath,
-  },
-});
-console.log(`${container.id} container ready`);
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
+// Initialize Alchemy
 const settings = {
   apiKey: process.env.ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 const alchemy = new Alchemy(settings);
 
+// Setup express to use cors and return json
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+/// API Endpoints
+
+// GET /api/nfts/?sessionId=...
+// Query Parameters:
+//   sessionId: string - the sessionId generated on login
+// Return the nfts for the account associated with the sessionId
+// Will also use cached response for 5 minutes
 app.get("/nfts", async (req, res) => {
   const { sessionId } = req.query;
 
-  const query = {
-    query: `SELECT c.id
-    FROM c
-    JOIN s IN c.sessions
-    WHERE s.sessionId = '${sessionId}'`,
-  };
+  const result = await accounts.findOne({
+    "sessions.sessionId": sessionId,
+  });
 
-  const result = await container.items.query(query).fetchAll();
-  if (result.resources.length > 0) {
-    const account = result.resources[0].id;
-
-    const nfts = alchemy.nft.getNftsForOwner(account).then(
-      async (nfts) => {
-        res.json(nfts.ownedNfts);
-        // await container.items.upsert({
-        //   id: account,
-        //   account: account,
-        //   ownedNfts: nfts.ownedNfts,
-        // });
-      },
-      (err) => {
-        res.json(err);
-      }
-    );
+  if (result) {
+    const account = result.id;
+    const minutes5 = 5 * 60 * 1000;
+    if (result.lastSynced && result.lastSynced > Date.now() - minutes5) {
+      res.json(result.nfts);
+    } else {
+      alchemy.nft.getNftsForOwner(account).then(
+        async (nfts) => {
+          res.json(nfts.ownedNfts);
+          await accounts.updateOne(
+            { id: account },
+            { $set: { nfts: nfts.ownedNfts, lastSynced: new Date() } }
+          );
+        },
+        (err) => {
+          res.json(err);
+        }
+      );
+    }
   } else {
     res.status(403).json([]);
   }
 });
 
+// POST /api/validateWalletLogin/
+// Body:
+//   address: string - the self reported address
+//   message: string - the generated message that the wallet signed
+//   signature: string - the signature of the wallet signed message
+// Validate the signature from the wallet login and return a sessionId
+// If resolved address does not match the address in the request, fail and let user try again
 app.post("/validateWalletLogin", async (req, res) => {
   const { address, message, signature } = req.body;
 
@@ -92,22 +92,26 @@ app.post("/validateWalletLogin", async (req, res) => {
       Math.random().toString(36).substring(2, 15) +
       Math.random().toString(36).substring(2, 15);
 
-    await container.items.upsert({
-      id: address,
-      account: address,
-      sessions: [
-        {
-          sessionId: sessionId,
-          timestamp: Date.now(),
+    await accounts.updateOne(
+      { id: address },
+      {
+        $push: {
+          sessions: {
+            sessionId: sessionId,
+            timestamp: new Date(),
+          },
         },
-      ],
-    });
+      },
+      { upsert: true }
+    );
+
     res.json({ sessionId: sessionId, success: true });
   } else {
     res.json({ success: false });
   }
 });
 
+// Start the server on the specified port
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
